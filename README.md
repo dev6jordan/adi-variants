e8987ba3cb71232b2ae2af1b48af775584902e41
Jordan McRae <jordanm@newyyz.com>
2016-07-07 09:55:30 -0400

    v0.0.0

---
+++ README.md
@@ -0,0 +1,59 @@
+# Adidas Variants
+Variant management directive for use across adidas applications.
+- - - -
+###Installation
+#####1) Install with bower
+`bower install git@github.com:dev6jordan/adi-variants.git --save`
+#####2) Add 'adidas.variants' as a dependency to the application
+`angular.module('myApp', ['adidas.variants']);`
+
+- - - -
+###Directive use example
+`<variants params="model.searchParams" userid="model.userID" app-name="'AI'" main-loader="model.showProgress"></variants>`        
+
+**1) params** -> List of application / report params that are to be updated and saved via variant system    
+**2) userid** -> User ID of current logged in user    
+**3) app-name** -> Shorthand of current application or report...    
+
+Value  | App Name
+------------- | -------------
+'AI'          | Account Inquiry
+'UPC'         | UPC Download
+'ST'          | Shipment Tracking
+'OT'          | Order Tracking
+'B2B'         | B2B Orders    
+**4) main-loader** -> Loading spinner boolean for main application
+- - - -
+
+###Functionality
+The variant management system provides users with the ability to save and load search presets based on their own preferences or 
+the preferences of other users. If a user decides to save their own variant based on their current search settings, the system will 
+check to see if this is a variant that has already been created and loaded by that user. If it already exists, the user is provided with
+a prompt which allows them to overwrite the current variant. If they decline, a modal is provided with the following fields: 
+
+- **Variant Name** -> An identifier for loading this variant in the future    
+- **Description** -> A short description of what the variant entails    
+- **Make Public** -> A boolean which will either make this variant public to other users, or private to only be used by that user  
+
+If no variant has been loaded or if the user has loaded a variant created by another user, they are not prompted to overwrite the 
+variant and are immediately directed to the variant save modal.
+
+If a user clicks the variant lookup icon, a modal is provided with a list of all public variants for the current application. 
+The user can search for a variant via the following fields:
+
+- **Variant Name** -> The name of the variant
+- **Description** -> The variant's description
+- **User** -> The user who created the variant
+
+The list is immediately filtered based on the user's search preferences. The user can click on the 'select' button to load that variant's
+search parameters into the current application search screen.
+
+
+###Error / Success Messages
+
+Upon error or success of certain actions, adi-variants emits two events:
+
+**1) variantSuccessAlert** -> Emitted when an action has been completed successfully    
+**2) variantErrorAlert** -> Emitted when an action has not been completed successfully    
+e.g.:    
+`$rootScope.$on('variantSuccessAlert', function (event, message) { alert(message)  });`
---
+++ variants.js
@@ -0,0 +1,336 @@
+'use strict';
+angular.module('adidas.variants', []);
+
+angular.module('adidas.variants')
+  .directive('variants', function () {
+    return {
+      templateUrl: 'bower_components/adidas.variants/variants.html',
+      restrict: 'E',
+      scope: {
+        params: '=',
+        userid: '=',
+        appName: '=',
+        mainLoader: '='
+      },
+      controller: function ($scope, $modal, $timeout, VariantService, $rootScope) {
+        $scope.saveDisabled = true;
+        $scope.variant = [];
+        $scope.showProgress = false;
+
+        $scope.save = function () {
+          if ($scope.saveDisabled) {
+            return;
+          }
+          // If already exists...
+          if (($scope.userid === $scope.variant.createdBy) && $scope.variant.id!=='') {
+            var confirmOverwrite = (confirm('Would you like to overwrite the current search?'));
+            if (confirmOverwrite) {
+              $scope.mainLoader = true;
+              VariantService.updateVariant($scope.userid, $scope.variant)
+                .then(function success (response) {
+                  $rootScope.$emit('variantSuccessAlert', 'Search has been saved successfully.');
+                  $scope.mainLoader = false;
+                }, function err (response) {
+                  $rootScope.$emit('variantErrorAlert', 'Search could not be saved.');
+                  $scope.mainLoader = false;
+                });
+            } else {
+              $scope.openVariantConfig();
+            }
+          } else {
+            $scope.openVariantConfig();
+          }
+        };
+
+        $scope.openVariantConfig = function () {
+          var loadInstance = $modal.open({
+            templateUrl: 'bower_components/adidas.variants/variant-config.html',
+            controller: 'VariantConfigCtrl',
+            size: 'md',
+            resolve: {
+              params: function () {
+                return $scope.params;
+              },
+              userid: function () {
+                return $scope.userid;
+              },
+              appName: function () {
+                return $scope.appName;
+              },
+              mainLoader: function () {
+                return $scope.mainLoader;
+              }
+            }
+          });
+
+          loadInstance.result.then(function (response) {
+            if (response !== undefined) {
+              $scope.variant = eval(response.data)[0];
+              $rootScope.$emit('variantSuccessAlert', 'Search has been saved successfully.');
+            }
+          }, function err () {
+            // Modal closed via backdrop click
+          });
+        };
+
+        $scope.loadVariants = function () {
+          var loadInstance = $modal.open({
+            templateUrl: 'bower_components/adidas.variants/variant-search.html',
+            controller: 'VariantCtrl',
+            size: 'md',
+            resolve: {
+              params: function () {
+                return $scope.params;
+              },
+              userid: function () {
+                return $scope.userid;
+              },
+              appName: function () {
+                return $scope.appName;
+              }
+            }
+          });
+
+          loadInstance.result.then(function (variant) {
+            $scope.params = variant.params;
+            $scope.variant = variant;
+          }, function err () {
+            // Modal closed via backdrop click
+          });
+        };
+
+        $scope.isSaveDisabled = function () {
+          return $scope.saveDisabled;
+        };
+
+        $timeout(function () {
+          $scope.$watch(function () {return $scope.params}, function (newV, oldV) {
+            if (newV !== oldV) {
+              $scope.saveDisabled = false;
+            }
+          }, true);
+        });
+      }
+    };
+  });
+
+  angular.module('adidas.variants')
+    .controller('VariantCtrl', function ($scope, params, userid, appName, VariantService, $window, $filter, $modalInstance) {
+      $scope.params = params;
+      $scope.userid = userid;
+      $scope.appName = appName;
+      $scope.showProgress = false;
+      $scope.results = '';
+      $scope.hasError = false;
+      $scope.errorMsg = '';
+      $scope.onlyMine = false;
+      $scope.model = {
+        name: '',
+        description: '',
+        userID: '',
+        filterBy: 'All',
+        currentPage: 1,
+        pageSize: 20,
+        pagesToShow: 5,
+        beginFrom: 0,
+        filteredList: []
+      };
+
+      $scope.init = function () {
+        $scope.showProgress = true;
+
+        VariantService.getVariantList()
+          .then(function success (response) {
+            $scope.results = eval(response.data);
+            $scope.searchList();
+          }, function err () {
+            $scope.hasError = true;
+            $scope.errorMsg = 'The system is busy and could not retrieve the list of searches. Please try again later.';
+          });
+      };
+
+      $scope.pageChange = function () {
+        $window.scrollTo(0, 0);
+
+        var changePage = ($scope.model.currentPage * $scope.model.pageSize) - $scope.model.pageSize;
+        $scope.model.beginFrom = changePage;
+      };
+
+      $scope.searchList = function () {
+        var nameUpper = $scope.model.name.toUpperCase();
+        var descriptionUpper = $scope.model.description.toUpperCase();
+        var userIdUpper = $scope.model.userID.toUpperCase();
+        if ($scope.onlyMine) {
+          $scope.model.userID = '';
+          userIdUpper = userid.toUpperCase();
+        }
+
+        $scope.model.filteredList = $filter('filter')($scope.results, function (item) {
+            if(item.variantName.toUpperCase().indexOf(nameUpper) !== -1 && item.shortDescription.toUpperCase().indexOf(descriptionUpper) !==-1 && item.createdBy.toUpperCase().indexOf(userIdUpper) !== -1){
+                return true;
+            } else {
+                return false;
+            }
+        });
+      };
+
+      $scope.selectVariant = function (variant) {
+        $scope.close(variant);
+      };
+
+      $scope.deleteVariant = function (variant) {
+        $scope.showProgress = true;
+        var confirmation = confirm('Are you sure you want to delete this search?');
+        if (confirmation) {
+          VariantService.deleteVariant(userid, variant)
+            .then(function success (response) {
+              $scope.results = response;
+              $scope.hasSuccess = true;
+              $scope.successMsg = 'Search has been successfully deleted.';
+              $scope.showProgress = false;
+            }, function err (response) {
+              $scope.hasError = true;
+              $scope.errorMsg = 'The system is busy and could not delete your search. Please try again later.';
+              $scope.showProgress = false;
+            });
+        }
+      };
+
+      $scope.close = function (params) {
+        if (params === undefined) {
+          $modalInstance.close($scope.params);
+        } else {
+          $modalInstance.close(params);
+        }
+      };
+
+      $scope.init();
+
+    });
+
+    angular.module('adidas.variants')
+      .directive('adiProgressBar', function (appVer) {
+        return {
+          templateUrl: 'bower_components/adidas.variants/variant-progress-bar.html'
+          // restrict: 'E'
+        };
+    });
+
+    angular.module('adidas.variants')
+      .controller('VariantConfigCtrl', function ($scope, VariantService, $window, $modalInstance, mainLoader) {
+        $scope.model = {
+          name: '',
+          description: '',
+          isPublic: false,
+        };
+        $scope.showProgress = false;
+
+        $scope.hasError = false;
+
+        $scope.saveVariant = function () {
+          $scope.hasError = false;
+          $scope.showProgress = true;
+          VariantService.saveNewVariant($scope.model)
+            .then(function success (response) {
+              $scope.showProgress = false;
+              $modalInstance.close(response);
+            }, function err (response) {
+              $scope.showProgress = false;
+              $scope.hasError = true;
+            });
+        };
+
+        $scope.close = function () {
+          $modalInstance.close();
+        };
+      });
+
+  angular.module('adidas.variants')
+    .factory('VariantService', function ($q, $http, $location) {
+      var localData = 'bower_components/adidas.variants/variants.txt';
+      var serviceUrl = '/appname=Portal_SAP&prgname=AJS_Variant_Lookup';
+      var absURL = $location.absUrl();
+      var localSaveSuccess = 'bower_components/adidas.variants/save_variant.txt';
+
+      return {
+        getVariantList: function () {
+          var deferred = $q.defer();
+          var url = localData;
+          if (absURL.indexOf(':9000') === -1) {
+            url = serviceUrl;
+          }
+          $http.get(url)
+            .then(function success (response) {
+              deferred.resolve(response);
+            }, function err (response) {
+              deferred.reject(response);
+            });
+          return deferred.promise;
+        },
+        updateVariant: function (id, newParams) {
+          var deferred = $q.defer();
+          var params = '&id=' + id + '&params=' + newParams;
+
+          if (absURL.indexOf(':9000') === -1) {
+            $http.post(serviceUrl, params.toString())
+              .then(function success (response) {
+                deferred.resolve(response);
+              }, function err (response) {
+                deferred.reject(response);
+              });
+          } else {
+            $http.get(localData)
+              .then(function (response) {
+                deferred.resolve(response);
+              });
+          }
+          return deferred.promise;
+        },
+        saveNewVariant: function (newParams) {
+          var deferred = $q.defer();
+          var params = '&params=' + newParams;
+
+          if (absURL.indexOf(':9000') === -1) {
+            $http.post(serviceUrl, params.toString())
+              .then(function success (response) {
+                if (response.data.status === 'Y') {
+                  deferred.resolve(response);
+                } else {
+                  deferred.reject(response);
+                }
+              }, function err (response) {
+                deferred.reject(response);
+              });
+          } else {
+            $http.get(localData)
+              .then(function (response) {
+                deferred.resolve(response);
+              });
+          }
+          return deferred.promise;
+        },
+        deleteVariant: function (id, variant) {
+          var deferred = $q.defer();
+          var params = '&params=' + variant;
+
+          if (absURL.indexOf(':9000') === -1) {
+            $http.post(serviceUrl, params.toString())
+              .then(function success (response) {
+                if (response.data.status === 'Y') {
+                  deferred.resolve(response);
+                } else {
+                  deferred.reject(response);
+                }
+              }, function err (response) {
+                deferred.reject(response);
+              });
+          } else {
+            $http.get(localData)
+              .then(function (response) {
+                deferred.resolve(response);
+              });
+          }
+          return deferred.promise;
+        }
+      };
+    });
